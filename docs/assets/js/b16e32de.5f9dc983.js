9a0490543167a08a5da58ead9a93d9cb34657784
Georg Wittberger <georg.wittberger@gmail.com>
2023-07-14 13:48:42 +0200

    docs: initial documentation

---
+++ docs/assets/js/b16e32de.5f9dc983.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmicrofrontend_template_docs=self.webpackChunkmicrofrontend_template_docs||[]).push([[542],{3769:e=>{e.exports=JSON.parse('{"name":"docusaurus-plugin-content-docs","id":"default"}')}}]);
