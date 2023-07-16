7e3662a2235d70068ec94df04eaa45de34d46893
Georg Wittberger <georg.wittberger@gmail.com>
2023-07-16 19:06:05 +0200

    chore: change colors and add social image

--- docs-src/docusaurus.config.js
+++ docs-src/docusaurus.config.js
@@ -36,6 +36,7 @@ const config = {
   themeConfig:
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     ({
+      image: "img/social.drawio.png",
       navbar: {
         title: "Microfrontend Template",
         items: [
